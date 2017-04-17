737266b10443e9982ac7a1309a0ce714833ebf6f
Aaron Nguyen and Jesse Hoare Evans <jesse@>
2017-04-17 16:49:05 -0700

    attempt at finishing leetspeak project

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -16,8 +16,36 @@ class String
       end
 
       if letter.include?("s")
-        letter.gsub!("s", "z")
+        if letter[0] == ("s")
+        (1..letter.length()-1).each do |i|
+          if letter[i] = "s"
+            letter[i].gsub!("s", "z")
+        end
       end
+        else letter.gsub!("s", "z")
+        end
+      end
+      #
+      # if letter.chars.first("s")
+      #
+      # if letter.chars.first("z")
+      #   letter.push(letter)
+
+
+
+      # if letter.chars.first?("s")
+      #   letter.push(letter)
+
+      # end
+
+      # if letter.chars.first("z")
+      #   letter.sub!("z", "s")
+      # # elsif split_sentence.first?("s")
+      # #   split_sentence.gsub!("s", "s")
+      # end
+      #
+      # if letter.index?("s")
+      #   letter.push("s")
     end # this closes "split_sentence" function
     split_sentence.join(" ")
   end # this closes "leetspeak" method
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -2,17 +2,17 @@ require ('rspec')
 require ('leetspeak')
 
   describe('String#leetspeak') do
-    it('replaces every "e" in a string with a "3"') do
-      expect("elephant".leetspeak).to(eq("3l3phant"))
-    end
-
-    it('replaces every "o" in a string with a "0"') do
-       expect("boo boo".leetspeak).to(eq("b00 b00"))
-     end
-
-    it('replaces every "I" in a string with a "1"') do
-       expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
-     end
+    # it('replaces every "e" in a string with a "3"') do
+    #   expect("elephant".leetspeak).to(eq("3l3phant"))
+    # end
+    #
+    # it('replaces every "o" in a string with a "0"') do
+    #    expect("boo boo".leetspeak).to(eq("b00 b00"))
+    #  end
+    #
+    # it('replaces every "I" in a string with a "1"') do
+    #    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+    #  end
 
 
     it('replaces every "s" in a string with a "z"') do
